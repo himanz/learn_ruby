767f01d5969f74e8159d788788d7bbf36aaddcbc
Jonathan Chang <jonoschang7@gmail.com>
2014-04-07 22:58:27 -0400

    Finish make_change

--- make_change/make_change.rb
+++ make_change/make_change.rb
@@ -1,7 +1,22 @@
 class Changer
 	def self.make_change(change)
-    if change == 25
-    	[25]
+    coins = []
+    amount = change
+    while amount > 0
+      if amount >= 25
+    	  coins << 25
+    	  amount -= 25
+    	elsif amount >= 10
+    		coins << 10
+    		amount -= 10
+    	elsif amount >= 5
+    		coins << 5
+    		amount -= 5
+    	else
+    		coins << 1
+    		amount -= 1
+      end
     end
+    coins
 	end
 end
